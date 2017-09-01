67a5ae5513fbb6e373340af0f751b8c4ad2cd26c
Tom Rozendaal <tom.rozendaal@finalist.nl>
2017-09-01 13:18:17 +0200

    Component props

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,13 +1,30 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
 
-import Example from '../../src'
+import ReactEmojiSelector from '../../src'
+import '../../src/index.css'
 
 class Demo extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      selectedEmoji: {emoji: '💯', key: '100'}
+    }
+  }
+
+  selectEmoji(e) {
+    this.setState({selectedEmoji: e})
+  }
+
   render() {
     return <div>
       <h1>react-emoji-selector Demo</h1>
-      <Example/>
+      <h2>{this.state.selectedEmoji.emoji}</h2>
+      <ReactEmojiSelector
+        visibleAmount={112}
+        searchPlaceholder='Search emoji' 
+        onSelect={(e) => this.selectEmoji(e)}/>
     </div>
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,33 +3,41 @@ import NodeEmoji from 'node-emoji'
 import { flatMap } from 'lodash'
 
 const DEFAULT_VISIBLE_AMOUNT = 90
+const DEFAULT_SEARCH_PLACEHOLDER = 'Search'
 
 export default class ReactEmojiSelector extends React.Component {
 
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
+      visibleAmount: props.visibleAmount || DEFAULT_VISIBLE_AMOUNT,
+      searchPlaceholder: props.searchPlaceholder || DEFAULT_SEARCH_PLACEHOLDER,
       showMore: false,
       query: ''
     }
   }
 
+  select(e) {
+    const emoji = NodeEmoji.find(e)
+    this.props.onSelect(emoji)
+  }
+
   render() {
-    const { showMore, query } = this.state
+    const { showMore, query, visibleAmount, searchPlaceholder } = this.state
     let emojiList = flatMap(NodeEmoji.search(query), (e) => e.emoji)
 
     return (
       <div className='react-emoji-selector'>
-        <input className='search-emoji' type='text' placeholder='Search emoji' onChange={(e) => this.setState({query: e.target.value.toLowerCase()})} />
+        <input className='react-emoji-selector-search' type='text' placeholder={searchPlaceholder} onChange={(e) => this.setState({query: e.target.value.toLowerCase()})} />
         { emojiList.map((emoji, index) => {
-          if (showMore === false && index >= DEFAULT_VISIBLE_AMOUNT) return false
+          if (showMore === false && index >= visibleAmount) return false
           return (
-            <div key={index} className='badge-selector' onClick={() => this.props.changeBadge(emoji, BADGE_TYPE_EMOJI)}>
+            <div key={index} className='react-emoji-selector-emoji' onClick={() => this.select(emoji)}>
               {emoji}
             </div>
           )
         })}
-        { (!showMore && emojiList.length > DEFAULT_VISIBLE_AMOUNT) && <div className='show-more'><button onClick={ () => this.setState({showMore: !showMore}) }>Show more</button></div> }
+        { (!showMore && emojiList.length > visibleAmount) && <div className='react-emoji-selector-show-more'><button onClick={ () => this.setState({showMore: !showMore}) }>Show more</button></div> }
       </div>
     )
   }
