22cf6415b480f04757837fc26620a2b29c8cf08a
Tom Rozendaal <tom.rozendaal@finalist.nl>
2017-09-01 15:09:03 +0200

    Tests

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,23 +1,43 @@
-import expect from 'expect'
 import React from 'react'
-import {render, unmountComponentAtNode} from 'react-dom'
 
-import Component from 'src/'
+import {shallow, mount, render} from 'enzyme'
+import {expect} from 'chai'
+import sinon from 'sinon'
 
-describe('Component', () => {
-  let node
+import ReactEmojiSelector from 'src/'
 
-  beforeEach(() => {
-    node = document.createElement('div')
+describe('ReactEmojiSelector', () => {
+
+  it('to have 10 emojis when specified', () => {
+      const wrapper = shallow(<ReactEmojiSelector visibleAmount={10} />);
+      expect(wrapper.find('.react-emoji-selector-emoji')).to.have.length(10)
   })
 
-  afterEach(() => {
-    unmountComponentAtNode(node)
+  it('to have 90 emojis when no amount specified', () => {
+      const wrapper = shallow(<ReactEmojiSelector />);
+      expect(wrapper.find('.react-emoji-selector-emoji')).to.have.length(90)
   })
 
-  it('displays a welcome message', () => {
-    render(<Component/>, node, () => {
-      expect(node.innerHTML).toContain('Welcome to React components')
-    })
+  it('returns the selected emoji', () => {
+      const buttonClick = sinon.spy();
+      const wrapper = shallow(
+        <ReactEmojiSelector onSelect={buttonClick} visibleAmount={1}/>
+      );
+      wrapper.find('.react-emoji-selector-emoji').simulate('click');
+      expect(buttonClick.calledOnce).to.equal(true);
+  })
+
+  it('renders all emojis when show more button is pressed', () => {
+      const wrapper = shallow(<ReactEmojiSelector visibleAmount={10}/>)
+      expect(wrapper.find('.react-emoji-selector-emoji')).to.have.length(10)
+      wrapper.find('.react-emoji-selector-show-more button').simulate('click')
+      expect(wrapper.find('.react-emoji-selector-emoji')).length.to.be.above(1300)
+  })
+
+  it('renders one emoji when rocket is searched for', () => {
+      const wrapper = shallow(<ReactEmojiSelector visibleAmount={10}/>)
+      expect(wrapper.find('.react-emoji-selector-emoji')).to.have.length(10)
+      wrapper.find('.react-emoji-selector-search').simulate('change', {target: {value: 'rocket'}});
+      expect(wrapper.find('.react-emoji-selector-emoji')).to.have.length(1)
   })
 })
