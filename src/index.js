27512440f66a56a40998aa1d4a2dab26f7d265d7
Tom Rozendaal <tom.rozendaal@finalist.nl>
2017-09-01 11:42:13 +0200

    Component setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
 import React, {Component} from 'react'
+import NodeEmoji from 'node-emoji'
+import { flatMap } from 'lodash'
 
-export default class extends Component {
+const DEFAULT_VISIBLE_AMOUNT = 90
+
+export default class ReactEmojiSelector extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      showMore: false,
+      query: ''
+    }
+  }
+
   render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+    const { showMore, query } = this.state
+    let emojiList = flatMap(NodeEmoji.search(query), (e) => e.emoji)
+
+    return (
+      <div className='react-emoji-selector'>
+        <input className='search-emoji' type='text' placeholder='Search emoji' onChange={(e) => this.setState({query: e.target.value.toLowerCase()})} />
+        { emojiList.map((emoji, index) => {
+          if (showMore === false && index >= DEFAULT_VISIBLE_AMOUNT) return false
+          return (
+            <div key={index} className='badge-selector' onClick={() => this.props.changeBadge(emoji, BADGE_TYPE_EMOJI)}>
+              {emoji}
+            </div>
+          )
+        })}
+        { (!showMore && emojiList.length > DEFAULT_VISIBLE_AMOUNT) && <div className='show-more'><button onClick={ () => this.setState({showMore: !showMore}) }>Show more</button></div> }
+      </div>
+    )
   }
 }
